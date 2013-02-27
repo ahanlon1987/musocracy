7665da2e5214eadc494e0ce06ee851596f8975f1
Aaron Hanlon <ahanlon@gmail.com>
2013-02-27 16:36:47 -0600

    removing musocracyapp folder

--- musocracyapp/config.groovy
+++
@@ -1,8 +0,0 @@
-environments {
-
-    local {
-        warName = 'musocracyapp.war'
-        warDestinationDir =  'C:/musocracy/tomcat/apache-tomcat-7.0.37/webapps'
-        configLocation = 'C:/musocracy/config'
-    }
-}
