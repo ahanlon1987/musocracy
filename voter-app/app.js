6bc85e471ced481ec720a2ff49fdbc86b9eeacf5
pmaccart <pmaccart@alumni.nd.edu>
2013-02-27 11:54:25 -0600

    Add simple handler for searching by track

--- voter-app/app.js
+++ voter-app/app.js
@@ -6,6 +6,7 @@
 var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
+  , search = require('./routes/search')
   , http = require('http')
   , path = require('path');
 
@@ -31,6 +32,7 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/search/track', search.byTrack);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
