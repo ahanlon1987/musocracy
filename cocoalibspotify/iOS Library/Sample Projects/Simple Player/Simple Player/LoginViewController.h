56fdd20331342ddb36c7c1b2c4ea69f5ada93880
pmaccart <pmaccart@alumni.nd.edu>
2013-03-06 22:47:27 -0600

    shit works
    commit before i break it

--- cocoalibspotify/iOS Library/Sample Projects/Simple Player/Simple Player/LoginViewController.h
+++ cocoalibspotify/iOS Library/Sample Projects/Simple Player/Simple Player/LoginViewController.h
@@ -2,15 +2,12 @@
 //  LoginViewController.h
 //  Simple Player
 //
-//  Created by Phil MacCart on 3/2/13.
+//  Created by Phil MacCart on 3/5/13.
 //  Copyright (c) 2013 Spotify. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
-#import "CocoaLibSpotify.h"
 
-@interface LoginViewController : UIViewController <SPSessionDelegate>
-@property (weak, nonatomic) IBOutlet UITextField *username;
-@property (weak, nonatomic) IBOutlet UITextField *password;
+@interface LoginViewController : UIViewController
 
 @end
--- cocoalibspotify/iOS Library/Sample Projects/Simple Player/Simple Player/LoginViewController.m
+++ cocoalibspotify/iOS Library/Sample Projects/Simple Player/Simple Player/LoginViewController.m
@@ -2,21 +2,17 @@
 //  LoginViewController.m
 //  Simple Player
 //
-//  Created by Phil MacCart on 3/2/13.
+//  Created by Phil MacCart on 3/5/13.
 //  Copyright (c) 2013 Spotify. All rights reserved.
 //
 
 #import "LoginViewController.h"
-#include "appkey.c"
 
 @interface LoginViewController ()
 
 @end
 
 @implementation LoginViewController
-
-@synthesize username;
-@synthesize password;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -27,27 +23,10 @@
     return self;
 }
 
-- (IBAction)loginPressed:(id)sender {
-    
-    NSString * usernameVal = self.username.text;
-    NSString * passwordVal = self.password.text;
-    
-    NSLog(@"Username: %@, Password: %@", usernameVal, passwordVal);
-    
-    NSError *error = nil;
-    [SPSession initializeSharedSessionWithApplicationKey:[NSData dataWithBytes:&g_appkey
-                                                                        length:g_appkey_size] userAgent:@"com.spotify.SimplePlayer-iOS"
-                                           loadingPolicy:SPAsyncLoadingManual
-                                                   error:&error];
-    
-    
-    
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+    // Do any additional setup after loading the view from its nib.
 }
 
 - (void)didReceiveMemoryWarning
@@ -56,9 +35,4 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void)viewDidUnload {
-    [self setUsername:nil];
-    [self setPassword:nil];
-    [super viewDidUnload];
-}
 @end
