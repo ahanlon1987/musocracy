ebe46ba895832d8167e3ad354353b5b95706f4b3
pmaccart <pmaccart@alumni.nd.edu>
2013-03-18 09:28:40 -0500

    add grunt task for watching hogan templates for changes

--- voter-app/Gruntfile.js
+++ voter-app/Gruntfile.js
@@ -1,129 +1,98 @@
-module.exports = function(grunt) {
-
-    // replace this line with
-    // grunt.loadNpmTasks("require-js");
-    // if you use this example standalone
-//    grunt.loadTasks("../../tasks");
+module.exports = function (grunt) {
 
     grunt.initConfig({
 
-        // requirejs: {
-        //     std: {
-        //         options: {
-        //             appDir: 'public2',
-        //             mainConfigFile: 'public2/js/config.js',
-        //             baseUrl: 'js/lib',
-        //             dir: 'public2-built',
-        //             paths: {
-        //                 app2: '../app2'
-        //             },
-        //             // name: 'app2',
-        //             // include: ['app2']
-        //             modules: [  
-        //                 {
-        //                     name: "../app2",
-        //                     include: [
-        //                         "jquery"
-        //                     ]
-        //                 }
-        //             ] 
-        //         }
-        //     }
-        // },
 
-    requirejs: {
-      compile: {
-        options: {
-          appDir: 'public',
-          baseUrl: 'js',
-          paths: {
-              // app: '../mobile'
-              // jquery: "jquery-1.9.1",
+        requirejs:{
+            compile:{
+                options:{
+                    appDir:'public',
+                    baseUrl:'js',
+                    paths:{
+                        // app: '../mobile'
+                        // jquery: "jquery-1.9.1",
 
-          },
-          
-          dir: 'public-built',
-          
-          mainConfigFile: 'public/js/mobile.js',
+                    },
 
-          modules: [
-            //First set up the common build layer.
-            {
-              //module names are relative to baseUrl
-              name: 'mobile'
-              //List common dependencies here. Only need to list
-              //top level dependencies, "include" will find
-              //nested dependencies.
-              // include: [
-                // 'jquery', 'underscore', 'backbone'
-              // ]
+                    dir:'public-built',
+
+                    mainConfigFile:'public/js/app.js',
+
+                    modules:[
+                        {
+                            name:'app'
+                        }
+                    ]
+                }
             }
-          ]
-        }
-      }
-    },
+        },
 
-        hogan: {
-            compile: {
-                options: {
+        hogan:{
+            compile:{
+                options:{
                     prettify:true,
                     amdWrapper:true,
-                    defaultName:function(filename) {
+                    defaultName:function (filename) {
                         var name = filename.replace("public/templates/", "");
                         return name.replace(".hogan", "");
                     }
                 },
-                files: {
-                    "public/js/templates.js": ["public/templates/**/*.hogan"]
+                files:{
+                    "public/js/templates.js":["public/templates/**/*.hogan"]
                 }
             }
         },
 
-        less: {
-            compile: {
-                options: {
+        less:{
+            compile:{
+                options:{
                     paths:['public/less']
                 },
-                files: {
-                    'public/css/app.css': 'public/less/app.less'
+                files:{
+                    'public/css/app.css':'public/less/app.less'
                 }
             }
         },
 
-        cssmin: {
-            compile: {
-                files: {
-                    'public-built/css/app.css': ['public/css/app.css']
+        cssmin:{
+            compile:{
+                files:{
+                    'public-built/css/app.css':['public/css/app.css']
                 }
             }
         },
 
-        watch: {
-            resources: {
-                files: ['**/*.js', 'public/less/**/*.less', 'public/html/**/*.html'],
-                tasks: ['dev-release'],
-                options: {
-                    debounceDelay: 250
-                }
-            }
-        },
-
-        jshint: {
-            options: {
-                curly: true,
-                eqeqeq: true,
-                immed: true,
-                latedef: true,
-                newcap: true,
-                noarg: true,
-                sub: true,
-                undef: true,
-                eqnull: true,
-                browser: true,
-                nomen: true
+        watch:{
+            templates: {
+                files:['public/templates/**/*.hogan'],
+                tasks:['hogan']
             },
-            globals: {
-                define: true
+
+            resources:{
+                files:['**/*.js', 'public/less/**/*.less', 'public/html/**/*.html'],
+                tasks:['dev-release'],
+                options:{
+                    debounceDelay:250
+                }
+            }
+        },
+
+        jshint:{
+            options:{
+                curly:true,
+                eqeqeq:true,
+                immed:true,
+                latedef:true,
+                newcap:true,
+                noarg:true,
+                sub:true,
+                undef:true,
+                eqnull:true,
+                browser:true,
+                nomen:true
+            },
+            globals:{
+                define:true
             }
         }
     });
@@ -135,12 +104,13 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-hogan');
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
     // grunt.loadNpmTasks("grunt-connect");
 
 
     grunt.registerTask('dev-release', 'less:development');
 
     grunt.registerTask('default', ['jshint', 'requirejs:std']);
-  grunt.registerTask('build', ['requirejs:compile',  'less:compile', 'cssmin:compile', 'hogan:compile']);
+    grunt.registerTask('build', ['requirejs:compile', 'less:compile', 'cssmin:compile', 'hogan:compile']);
 
 };
